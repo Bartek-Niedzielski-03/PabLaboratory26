7f1c99a6df2a53a5ecc4b645fe124b582e6db2a1
bartlomiej.niedziels <bartlomiej.niedziels@microsoft.wsei.edu.pl>
2026-03-04 11:22:30 +0100

    end of lab 02

---
+++ AppCore/Entities/Address.cs
@@ -0,0 +1,12 @@
+using AppCore.Enums;
+
+namespace AppCore.Entities;
+
+public class Address
+{
+    public string Street { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public AddressType Type { get; set; } = AddressType.Main;
+}
